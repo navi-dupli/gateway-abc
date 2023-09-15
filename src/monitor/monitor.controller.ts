6351ddcc454562dd442ac38724b7b20f5a433d3f
plinio <p.grijalbar@uniandes.edu.co>
2023-09-15 12:30:00 -0500

    Iscription, shutdown and health

--- src/monitor/monitor.controller.ts
+++ src/monitor/monitor.controller.ts
@@ -7,8 +7,19 @@ export class MonitorController {
     }
 
     @Post()
-    create(@Body() data): Promise<any> {
-        return this.firebase.firestore.collection('monitor').add({...data, timestamp: Date.now()});
+    create(@Body() body): Promise<any> {
+        // The message is a unicode string encoded in base64.
+        const message = JSON.parse(Buffer.from(body.message.data, 'base64').toString(
+            'utf-8'
+        ));
+        if (message.type === 'inscription') {
+            return this.firebase.firestore.collection('intances').doc(message.instance).set({...message, timestampRegister: Date.now(), status: 'active'});
+        } else if (message.type === 'shutdown') {
+            const instance = this.firebase.firestore.collection('intances').doc(message.instance);
+            return instance.update({state: true});
+        } else {
+            return this.firebase.firestore.collection('monitor').add({...message, timestampRegister: Date.now()});
+        }
     }
 
 }
