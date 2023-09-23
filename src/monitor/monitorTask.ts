8f29397f03082469af997462ee7a84c38a4e13a4
plinio <p.grijalbar@uniandes.edu.co>
2023-09-23 02:13:15 -0500

    monitor

--- src/monitor/monitorTask.ts
+++ src/monitor/monitorTask.ts
@@ -10,40 +10,37 @@ export class MonitorTask {
     async handleCron() {
         const instancesRef = this.firebase.firestore.collection('intances');
         const serviceRef = this.firebase.firestore.collection('services');
-        const snapshot = await instancesRef.where('status', "==", 'active').orderBy('timestampRegister', 'desc').limit(10).get();
+        const snapshot = await instancesRef.where('status', "==", 'active').get();
         const docs = snapshot.docs.map(async (doc) => {
             const instanceData = doc.data();
             const health = await this.getStatusInstance(instanceData.instance);
             await instancesRef.doc(doc.id).update({
-                health: health.disponibility, instance: instanceData.instance, timestampRegister: new Date()
+                health: health.availability, instance: instanceData.instance, timestampRegister: new Date()
             });
             return health;
         });
         const results = await Promise.all(docs);
-        const disponibility = results.reduce((a, b) => a + b.disponibility, 0) / results.length;
-        if (disponibility) {
-            const service = await serviceRef.add({service: 'micro-evaluaciones', disponibility, timestampRegister: Date.now()});
+        let availability = results.reduce((a, b) => a + b.availability, 0);
+        availability = availability > 0 ?  availability/ results.length : 0;
+        if (availability > 0.9) {
+            await serviceRef.add({service: 'micro-evaluaciones', availability: 1, timestampRegister: new Date(), healthData: results});
+        } else if (availability > 0.7) {
+            await serviceRef.add({service: 'micro-evaluaciones', availability: 0, timestampRegister: new Date(), healthData: results});
         } else {
-            const service = await serviceRef.orderBy('timestampRegister', 'desc').limit(1).get();
-            if (!service.empty) {
-                const serviceData = service.docs[0].data();
-                if (serviceData.disponibility > -1) {
-                    await serviceRef.add({service: 'micro-evaluaciones', disponibility: -1, timestampRegister: new Date()});
-                }
-            }
+            await serviceRef.add({service: 'micro-evaluaciones', availability: -1, timestampRegister: new Date(), healthData: results});
         }
     }
     async getStatusInstance(instance: any) {
         const healthRef = this.firebase.firestore.collection('monitor');
         const snapshot = await healthRef.where('instance', "==", instance).orderBy('timestampRegister', 'desc').limit(10).where('health', '==', 'down').get();
         const data = snapshot.docs.map((doc) => doc.data());
-        const instanceDisponibility = 10 - (snapshot.size / 10);
-        if (instanceDisponibility > 0.9) {
-            return { disponibility: 1, data};
-        } else if (instanceDisponibility > 0.7) {
-            return { disponibility: 0, data};
+        const instanceAvailability = 1 - (snapshot.size / 10);
+        if (instanceAvailability > 0.9) {
+            return { availability: 1, data};
+        } else if (instanceAvailability > 0.7) {
+            return { availability: 0, data};
         } else {
-            return { disponibility: -1, data};
+            return { availability: -1, data};
         }
     }
 
